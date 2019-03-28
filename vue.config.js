71cb585caf68c70b458cb550287967a7039d620d
Anatoli Makarevich <makaroni4@gmail.com>
2019-03-29 00:26:26 +0100

    Attempt #4, all good this time

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '<page-speed-insights-dashboard>'
+    ? '/page-speed-insights-dashboard/'
     : '/',
   css: {
     loaderOptions: {
